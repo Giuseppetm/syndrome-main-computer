191633fca27102e29afb82d5bb62736770fb16cd
Giuseppetm <gdeambo@yahoo.it>
2022-01-23 15:25:04 +0100

    Navigator for supers

--- src/Views/Supers.jsx
+++ src/Views/Supers.jsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom";
+import SupersImage from '../Images/supers.svg';
+import Search from '../Images/search.svg';
+import Fade from 'react-reveal/Fade';
 
 const Supers = () => {
     return (
         <section id="supers">
+            <div className="box-icons-borders" />
+            <div className="navigator-frame">
+                <Fade>
+                    <div className="navigator-item">
+                        <Link to="/supers/0">
+                            <span className="navigator-item-content"><img src={SupersImage} alt="Supers" className="icon" />Supers list</span>
+                        </Link>
+                    </div>
+                </Fade>
 
-            <Link to={`/supers/0`}>
-                Supers list
-            </Link>
-            
+                <Fade>
+                    <div className="navigator-item">
+                        <Link to="/search_supers">
+                            <span className="navigator-item-content"><img src={Search} alt="Search super" className="icon" />Search super</span>
+                        </Link>
+                    </div>
+                </Fade>
+            </div>
         </section>
     );
 };
