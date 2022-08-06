48cb4f4b14d601128e3f86425ed813828beee6bf
Giuseppetm <giuseppe.delcampo@outlook.com>
2022-08-06 12:16:57 +0200

    Scroll navigator items with keyboard
    Closes #8

--- src/Views/Navigator.jsx
+++ src/Views/Navigator.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-router-dom';
 import Dollar from '../Images/dollar.svg';
@@ -6,6 +7,42 @@ import Omnidroid from '../Images/omnidroid.svg';
 import Supers from '../Images/supers.svg';
 
 const Navigator = () => {
+    let [navItem, setNavItem] = React.useState(null);
+    let navItems = 4;
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setNavItem(0);
+        }, 800);
+    }, []);
+
+    React.useEffect(() => {
+        const downHandler = (event) => {
+            if (event.keyCode === 40) {
+                setNavItem((navItem + 1) % navItems);
+            }
+        };
+
+        const upHandler = (event) => {
+            if (event.keyCode === 38) {
+                setNavItem(((navItem - 1 + navItems) % navItems));
+            }
+        };
+
+        window.addEventListener("keydown", downHandler);
+        window.addEventListener("keyup", upHandler);
+
+        return () => {
+            window.removeEventListener("keydown", downHandler);
+            window.removeEventListener("keyup", upHandler);
+        };
+    }, [navItem, navItems]);
+
+    const buildClassName = (key) => {
+        if (key === navItem) return "navigator-item-content navigator-item-content-active";
+        return "navigator-item-content";
+    };
+
     return (
         <section id="navigator">
 
@@ -15,7 +52,7 @@ const Navigator = () => {
                 <Fade>
                     <div className="navigator-item">
                         <Link to="/kronos">
-                            <div className="navigator-item-content">
+                            <div className={buildClassName(0)} onMouseEnter={() => setNavItem(0)}>
                                 <img src={Isle} alt="Isle" className="icon" />
                                 Island operations
                             </div>
@@ -25,7 +62,7 @@ const Navigator = () => {
 
                 <Fade>
                     <div className="navigator-item disabled">
-                        <div className="navigator-item-content">
+                        <div className={buildClassName(1)} onMouseEnter={() => setNavItem(1)}>
                             <img src={Dollar} alt="Finances" className="icon" />
                             Finances
                         </div>
@@ -34,7 +71,7 @@ const Navigator = () => {
 
                 <Fade>
                     <div className="navigator-item disabled">
-                        <div className="navigator-item-content">
+                        <div className={buildClassName(2)} onMouseEnter={() => setNavItem(2)}>
                             <img src={Omnidroid} alt="Omnidroid" className="icon" />
                             Omnidroid metatraining
                         </div>
@@ -44,7 +81,7 @@ const Navigator = () => {
                 <Fade>
                     <div className="navigator-item">
                         <Link to="/supers">
-                            <div className="navigator-item-content">
+                            <div className={buildClassName(3)} onMouseEnter={() => setNavItem(3)}>
                                 <img src={Supers} alt="Supers" className="icon" />
                                 Supers
                             </div>
--- src/Views/Supers.jsx
+++ src/Views/Supers.jsx
@@ -1,9 +1,46 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import SupersImage from '../Images/supers.svg';
 import Search from '../Images/search.svg';
 import Fade from 'react-reveal/Fade';
 
 const Supers = () => {
+    let [navItem, setNavItem] = React.useState(null);
+    let navItems = 2;
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setNavItem(0);
+        }, 800);
+    }, []);
+
+    React.useEffect(() => {
+        const downHandler = (event) => {
+            if (event.keyCode === 40) {
+                setNavItem((navItem + 1) % navItems);
+            }
+        };
+
+        const upHandler = (event) => {
+            if (event.keyCode === 38) {
+                setNavItem(((navItem - 1 + navItems) % navItems));
+            }
+        };
+
+        window.addEventListener("keydown", downHandler);
+        window.addEventListener("keyup", upHandler);
+
+        return () => {
+            window.removeEventListener("keydown", downHandler);
+            window.removeEventListener("keyup", upHandler);
+        };
+    }, [navItem, navItems]);
+
+    const buildClassName = (key) => {
+        if (key === navItem) return "navigator-item-content navigator-item-content-active";
+        return "navigator-item-content";
+    };
+
     return (
         <section id="navigator">
             <div className="box-icons-borders" />
@@ -11,7 +48,10 @@ const Supers = () => {
                 <Fade>
                     <div className="navigator-item">
                         <Link to="/supers/0">
-                            <span className="navigator-item-content"><img src={SupersImage} alt="Supers" className="icon" />Supers list</span>
+                            <div className={buildClassName(0)} onMouseEnter={() => setNavItem(0)}>
+                                <img src={SupersImage} alt="Supers" className="icon" />
+                                Supers list
+                            </div>
                         </Link>
                     </div>
                 </Fade>
@@ -19,7 +59,10 @@ const Supers = () => {
                 <Fade>
                     <div className="navigator-item">
                         <Link to="/search_super">
-                            <span className="navigator-item-content"><img src={Search} alt="Search super" className="icon" />Search super</span>
+                            <div className={buildClassName(1)} onMouseEnter={() => setNavItem(1)}>
+                                <img src={Search} alt="Search super" className="icon" />
+                                Search super
+                            </div>
                         </Link>
                     </div>
                 </Fade>
