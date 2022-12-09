c524c6001c7c8fefa99fbcdc0e8e5b2999dc9d3d
Giuseppetm <giuseppe.delcampo@outlook.com>
2022-12-09 21:49:27 +0100

    More accurate super search result
    Added Frozone original picture

--- src/Views/SearchSuperResult.jsx
+++ src/Views/SearchSuperResult.jsx
@@ -12,31 +12,31 @@ const SearchSuperResult = () => {
     React.useEffect(() => {
         setTimeout(() => {
             setRender(true);
-        }, 800);
+        }, 600);
     }, []);
 
     return (
         <div className="search-super-result-frame">
             <div className="container-fluid h-100">
                 <div className="row h-100">
-                    <div className="col-5 padded borded header" />
+                    <div className="col-6 padded borded header" />
 
-                    <div className="col-7 padded header" />
+                    <div className="col-6 padded header" />
 
-                    <div className="col-5 p-0 padded borded content image">
+                    <div className="col-6 p-0 padded borded content image">
                         <Fade duration={200}>
                             <img id="super-image" src={require('../Images/Search Results/' + superResult?.img)} alt={superResult?.img} />
                         </Fade>
                     </div>
 
-                    <div className="col-7 padded result content">
+                    <div className="col-6 padded result content">
                         <Fade duration={200}>
                             <div className="mb-3">
                                 <h1 className="text-uppercase">{superResult?.name}</h1>
                                 <h5>LOCATION: {render ? <Fade duration={200}><span className="warning-color">{superResult?.locationKnown ? <>KNOWN</> : <>UNKNOWN</>}</span></Fade> : <></>}</h5>
                             </div>
-                            <div className="description text-uppercase mb-3">
-                                <p>
+                            <div className="description text-uppercase">
+                                <p className="mb-2">
                                     {superResult?.description}
                                 </p>
                                 <p>
@@ -47,9 +47,9 @@ const SearchSuperResult = () => {
                         </Fade>
                     </div>
 
-                    <div className="col-5 padded borded footer" />
+                    <div className="col-6 padded borded footer" />
 
-                    <div className="col-7 padded footer" />
+                    <div className="col-6 padded footer" />
                 </div>
 
             </div>
