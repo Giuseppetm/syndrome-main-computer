3b088042229b2321d7f2a51ae8e540885c8601bb
Giuseppe Del Campo <giuseppe.delcampo@outlook.com>
2025-08-24 21:51:12 +0200

    Add preload again

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,15 @@ const nextConfig: NextConfig = {
           },
         ],
       },
+      {
+        source: '/images/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
     ]
   },
 }
