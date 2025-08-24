23796a0026316fdaae5c16981e86fa073feade17
Giuseppe Del Campo <giuseppe.delcampo@outlook.com>
2025-08-24 18:48:09 +0200

    Remove speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import KronosLayout from '@/layouts/kronos'
 import type { AppProps } from 'next/app'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { eurostile } from '@/assets/fonts'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const gaId = 'G-220V3ZRLLS'
@@ -12,7 +11,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider value={system}>
       <KronosLayout className={eurostile.className}>
-        <SpeedInsights />
         <Component {...pageProps} />
         <GoogleAnalytics gaId={gaId} />
       </KronosLayout>
