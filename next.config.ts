c1c7ca93dabfe9859c6d04804f50e51a1bcc6e68
Giuseppe Del Campo <giuseppe.delcampo@outlook.com>
2025-08-24 21:41:36 +0200

    Add headers to next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,28 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'],
   },
+  async headers() {
+    return [
+      {
+        source: '/favicon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/images/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
