7b0a0ed1f5c971e8b5d78d30010ac3a2afa859d0
Giuseppe Del Campo <giuseppe.delcampo@outlook.com>
2025-08-24 21:31:50 +0200

    Remove image assets preload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'],
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,6 @@
-import { omnidroids, supers, supersResult } from '@/data'
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  // Collect all image paths from supers + omnidroids
-  const preloadImages = [
-    ...omnidroids.map((o) => `/images/omnidroids/${o.img}`),
-    ...supers.map((s) => `/images/supers/${s.img}`),
-    ...supersResult.map((s) => `/images/search-results/${s.img}`),
-  ]
-
   return (
     <Html lang="en" suppressHydrationWarning>
       <Head>
@@ -19,11 +11,6 @@ export default function Document() {
         {/* Ads */}
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2626937566773847" crossOrigin="anonymous" />
         <meta name="google-adsense-account" content="ca-pub-2626937566773847" />
-
-        {/* Preload all static images */}
-        {preloadImages.map((src) => (
-          <link key={src} rel="preload" as="image" href={src} />
-        ))}
       </Head>
       <body>
         <Main />
