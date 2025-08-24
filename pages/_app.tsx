d81c62bedfd8537525d5bf3b9531c634e999eaec
Giuseppe Del Campo <giuseppe.delcampo@outlook.com>
2025-08-24 15:36:40 +0200

    Add vercel speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import KronosLayout from '@/layouts/kronos'
 import type { AppProps } from 'next/app'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { eurostile } from '@/assets/fonts'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const gaId = 'G-220V3ZRLLS'
@@ -11,6 +12,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider value={system}>
       <KronosLayout className={eurostile.className}>
+        <SpeedInsights />
         <Component {...pageProps} />
         <GoogleAnalytics gaId={gaId} />
       </KronosLayout>
