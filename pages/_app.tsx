a6608d921778f93162c191064def200d8c7e83ad
giuseppetm <giuseppe.delcampo@outlook.com>
2025-09-03 12:31:32 +0200

    Add vercel analytics and speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import system from '@/styles'
 import MainLayout from '@/layouts/main'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { eurostile } from '@/assets/fonts'
+import { Analytics as VercelAnalytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const gaId = 'G-220V3ZRLLS'
@@ -13,6 +15,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <MainLayout className={eurostile.className}>
         <Component {...pageProps} />
         <GoogleAnalytics gaId={gaId} />
+        <VercelAnalytics />
+        <SpeedInsights />
       </MainLayout>
     </ChakraProvider>
   )
